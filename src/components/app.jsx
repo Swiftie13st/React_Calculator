eae41b6ebd4eb467219fb02c902ad8478986a389
Bruce <swiftie13s@163.com>
2022-05-27 14:27:42 +0800

    add route content

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import NavBar from './navBar';
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./content/home";
+import Calculator from "./content/calculator";
+import Login from "./content/login";
+import Register from "./content/register";
+import NotFound from "./content/notfound";
 class APP extends Component {
     state = {  } 
     render() { 
         return (
           <React.Fragment>
-              <NavBar/>
+            <NavBar />
             <div className="container">
-              <h1>Hello world</h1>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/calculator" element={<Calculator />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/404" element={<NotFound />} />
+                <Route path="*" element={<Navigate replace to="/404" />} />
+              </Routes>
             </div>
           </React.Fragment>
         );
---
+++ src/components/content/calculator.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; 
+import Base from "./base";
+
+class Calculator extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <Base>计算器</Base>
+        );
+    }
+}
+ 
+export default Calculator;
